9bf550e55c57ec20cd4f8931bee1f43329fae7d6
oooldtoy <51742057+oooldtoy@users.noreply.github.com>
2024-02-28 11:19:07 +0800

    Create com.tencent.qqlite.ts
    添加qq极速版置顶更新窗口的跳过规则

---
+++ src/apps/com.tencent.qqlite.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.wps.moffice_eng',
+  name: 'WPS',
+  groups: [
+{
+  id: 'com.tencent.qqlite',
+  name: 'QQ极速版',
+  groups: [
+    {
+      key: 1,
+      name: '置顶更新',
+      activityIds: ['com.tencent.mobileqq.activity.SplashActivity'],
+      rules: [
+        {
+          matches: '[id="com.tencent.qqlite:id/close_top_bar"]',
+          snapshotUrls: ['https://i.gkd.li/import/14428054'],
+        },
+      ],
+    },
+  ],
+}
+  ],
+});
